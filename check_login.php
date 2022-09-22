641fecf6f0a9495f3d8738ba782022bf987f211d
patic457 <patic457@gmail.com>
2022-09-22 12:36:52 +0700

    1239

--- check_login.php
+++ check_login.php
@@ -1,4 +1,5 @@
-<?php //session_start();
+<?php 
+// session_start();
 include("db.php");
 
 $username_session = $_POST['a'];
--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php 
 
-$status = session_status();
-if($status == PHP_SESSION_NONE){
-    //There is no active session
-    session_start();
-}else
-if($status == PHP_SESSION_DISABLED){
-    //Sessions are not available
-}else
-if($status == PHP_SESSION_ACTIVE){
-    //Destroy current and start new one
-    session_destroy();
-    session_start();
-}
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
