1058f7c9476163f46c52c1ec38e556ec8240d631
patic457 <patic457@gmail.com>
2022-09-23 15:12:50 +0700

    Update add_building.php

--- add_building.php
+++ add_building.php
@@ -1,21 +1,43 @@
-<?php include"session.php"; ?>
+<?php include "session.php"; ?>
 <link rel="stylesheet" type="text/css" href="style_adminn.css" />
 <script language="javascript">
-$(function(){$("#bt").click(function(){var building_=$("#building_").attr("value");var floor_=$("#floor_").attr("value");var check_status_1='building';var check_status_2='add';
-$.post("check_admin.php",{building_: building_,floor_: floor_,check_status_1: check_status_1,check_status_2: check_status_2},function(data){$("#msg").html(data);});
-});
-});
+    $(function() {
+        $("#bt").click(function() {
+            var building_ = $("#building_").attr("value");
+            var floor_ = $("#floor_").attr("value");
+            var check_status_1 = 'building';
+            var check_status_2 = 'add';
+            $.post("check_admin.php", {
+                building_: building_,
+                floor_: floor_,
+                check_status_1: check_status_1,
+                check_status_2: check_status_2
+            }, function(data) {
+                $("#msg").html(data);
+            });
+        });
+    });
 </script>
-<body><table class="mana"><tr><td>
-<tr><th>เพิ่มข้อมูลใหม่</th></tr>
-<tr><td>
-<form id="f1" method="post" >
-ชื่ออาคาร : <input type="text" id="building_" name="building_" >
-</td><td>
-ชั้นทั้งหมด : <input name="floor_" id="floor_" type="text" maxlength="2" >
-<input type="button" id="bt" value="บันทึกข้อมูล"/>
-</form>
-</td></tr></table>
+
+<body>
+    <table class="mana">
+        <tr>
+            <td>
+        <tr>
+            <th>เพิ่มข้อมูลใหม่</th>
+        </tr>
+        <tr>
+            <td>
+                <form id="f1" method="post">
+                    ชื่ออาคาร : <input type="text" id="building_" name="building_">
+            </td>
+            <td>
+                ชั้นทั้งหมด : <input name="floor_" id="floor_" type="text" maxlength="2">
+                <input type="button" id="bt" value="บันทึกข้อมูล" />
+                </form>
+            </td>
+        </tr>
+    </table>
 
 
 
