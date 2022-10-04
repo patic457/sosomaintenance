4624cddc7816df8858b4c8991678d7ad428d6300
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-10-04 22:02:34 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
-<?php
-echo "Copyright 2022 @ AIS Ticket Tool";
-?>
+<head>
+    <?php
+    echo "Copyright 2022 @ AIS Ticket Tool";
+    ?>
+
+    <script>
+        function myFunction() {
+            alert('Hello');
+        }
+        setTimeout(myFunction, 3000);
+    </script>
+
+</head>
