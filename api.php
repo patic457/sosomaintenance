37e6e2b0a7abcfe53c4454b091164f46a3187086
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-10-04 11:00:56 +0700

    Update api.php

--- api.php
+++ api.php
@@ -30,8 +30,8 @@ $opts = array('http' =>
 );
 
 $data = json_decode(file_get_contents('php://input'), true);
-print_r($data);
-echo $data["operacion"];
+
+echo json_encode($data["operacion"]);
 
 $conn->close();
 
