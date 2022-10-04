e28a985a1870696c89a5c63e0c4a51646b9c9af8
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-10-04 10:50:26 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$servername = "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "de7p9prkwmh3kquo";
+$password = "j2batag6pvrfimgh";
+$dbname = "g3uky2wss1pv3jyv";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error."<hr>");
+} 
+
+$postdata = http_build_query(
+    array(
+        'var1' => 'some content',
+        'var2' => 'doh'
+    )
+);
+
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-Type: application/json',
+        'content' => $postdata
+    )
+);
+
+$data = json_decode(file_get_contents('php://input'), true);
+print_r($data);
+echo $data["operacion"];
+
+$conn->close();
+
+
+?>
+
+
+
