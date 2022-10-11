bf33b0b667871437a28d469b2a0d247a4a25b46f
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-10-12 01:06:35 +0700

    Update apiGetList.php

--- apiGetList.php
+++ apiGetList.php
@@ -60,7 +60,7 @@ $num = mysqli_num_rows($result);
 for ($q = 0; $q < $num; $q++) {
     $obj = new stdClass();
     $fetch = mysqli_fetch_array($result);
-    $obj = json_decode($fetch[$q]);
+    $obj = $fetch[$q]["list"];
     // $obj->id;
     // $obj->list;
     array_push($data, $obj);
