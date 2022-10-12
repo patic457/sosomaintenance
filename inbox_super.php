517cc959a679f51d1276afbe350c7d065f8942b1
patic457 <patic457@gmail.com>
2022-10-12 16:12:41 +0700

    Update inbox_super.php

--- inbox_super.php
+++ inbox_super.php
@@ -111,16 +111,15 @@ $status_temp =  $fetch_st['status'];*/
             $api_url = 'https://sosomaintenance.herokuapp.com/apiGetList.php';
             $json_data = file_get_contents($api_url);
             $response_data = json_decode($json_data);
-            var_dump($response_data);
-            // $list_data = $response_data->data;
+            $list_data = $response_data->data;
             // $user_data = array_slice($user_data, 0, 9);
-            // foreach ($list_data as $list) {
-
+            foreach ($list_data as $list) {
+                var_dump($list);
             //     echo "name: " . $user->employee_name;
             //     echo "<br />";
             //     echo "name: " . $user->employee_age;
             //     echo "<br /> <br />";
-            // }
+            }
         }
 
                 ?>
