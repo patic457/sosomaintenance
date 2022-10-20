938729f0e5f9848f931c7629cafe5fc36336e2b6
patic457 <patic457@gmail.com>
2022-10-20 17:30:26 +0700

    17

--- api.php
+++ api.php
@@ -8,6 +8,7 @@ $servername = "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "de7p9prkwmh3kquo";
 $password = "j2batag6pvrfimgh";
 $dbname = "g3uky2wss1pv3jyv";
+$table = "g3uky2wss1pv3jyv.webhooks";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,7 +18,35 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error . "<hr>");
 }
 
-function createWebhook($conn)
+function adaptorPagerduty($conn, $table)
+{
+    $sql = "SELECT * FROM " . $table;
+    $result = mysqli_query($conn, $sql);
+    $data = [];
+    $num = mysqli_num_rows($result);
+    for ($q = 0; $num > $q; $q++) {
+        $fetch = mysqli_fetch_array($result);
+        $jsondecode_tmp =  json_decode($fetch[1]);
+        if ($jsondecode_tmp != null) {
+            $message_tmp = $jsondecode_tmp->{'messages'};
+            $message_incident_tmp = $message_tmp[0]->{'incident'};
+
+            $obj = new stdClass();
+            $obj->incident_event = $message_tmp[0]->{'event'};
+            $obj->incident_status = $message_incident_tmp->status;
+            $obj->incident_number = intval($message_incident_tmp->{'incident_number'});
+            $obj->incident_title = $message_incident_tmp->{'title'};
+            $obj->incident_description = $message_incident_tmp->{'description'};
+            $obj->incident_created_at = $message_incident_tmp->{'created_at'};
+
+            $obj->notification_status = intval($fetch['notification_status']);
+            $obj->messagesLogs = $jsondecode_tmp;
+            array_push($data, $obj);
+        }
+    }
+}
+
+function createWebhook($conn, $table)
 {
 
     $postdata = http_build_query(
@@ -40,7 +69,7 @@ function createWebhook($conn)
     $json_string = json_encode($data);
 
     if (isset($json_string) || $json_string == '') {
-        $sql = "INSERT INTO g3uky2wss1pv3jyv.webhooks (id, list) VALUES(NULL, '$json_string');";
+        $sql = "INSERT INTO $table (id, list) VALUES(NULL, '$json_string');";
         // echo $sql;
         $query = mysqli_query($conn, $sql);
         $expression = $json_string;
@@ -48,6 +77,8 @@ function createWebhook($conn)
     }
 }
 
-createWebhook($conn);
+createWebhook($conn, $table);
+
+adaptorPagerduty($conn, $table);
 
 $conn->close();
